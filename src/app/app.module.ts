15b036ca36816196579f616c174d94db403b7460
Ricardo Orlando Castillo Olivera <ricardocastilloisc@gmail.com>
2020-11-13 18:59:29 -0500

    treams y links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AppRoutingRoutingModule } from './app.routing';
+import { TeamsComponent } from './pages/teams/teams.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    TeamsComponent
   ],
   imports: [
     BrowserModule,
